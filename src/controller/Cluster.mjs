bccd45516e7b84f62e3f5d0a2f3be6536e2e5321
Michael van der Weg <michael@joinbox.com>
2018-09-08 18:31:48 +0200

    style: reformat code to satisfy the linter requierments

--- src/controller/Cluster.mjs
+++ src/controller/Cluster.mjs
@@ -1,21 +1,26 @@
-'use strict';
-
-
-import {Controller} from 'rda-service';
-import ClusterStatus from '../ClusterStatus';
+import { Controller } from 'rda-service';
 import superagent from 'superagent';
 import type from 'ee-types';
 import logd from 'logd';
+import ClusterStatus from '../ClusterStatus.mjs';
 
 
 
 const log = logd.module('rda-coordinator');
 
 
-
+/**
+ * this is the brain of RDA, it controls the clusters and its modifications
+ */
 export default class ClusterController extends Controller {
 
-
+    /**
+     * @param      {Object}          arg1                 options
+     * @param      {array}           arg1.dataSources     array containing the name of available
+     *                                                    data sources (at the time of writing this
+     *                                                    is just the rda-sample-strorage)
+     * @param      {RegistryClient}  arg1.registryClient  The registry client
+     */
     constructor({
         dataSources,
         registryClient,
@@ -54,57 +59,82 @@ export default class ClusterController extends Controller {
     async create(request, response) {
         const data = request.body;
 
-        if (!data) response.status(400).send(`Missing request body!`);
-        else if (!type.object(data)) response.status(400).send(`Request body must be a json object!`);
-        else if (!type.string(data.dataSource)) response.status(400).send(`Missing parameter 'dataSource' in request body!`);
-        else if (!type.string(data.dataSet)) response.status(400).send(`Missing parameter 'dataSet' in request body!`);
-        else if (!this.dataSources.has(data.dataSource)) response.status(404).send(`The data source ${data.dataSource} was not found!`);
-        else {
+        if (!data) response.status(400).send('Missing request body!');
+        else if (!type.object(data)) {
+            response.status(400).send('Request body must be a json object!');
+        } else if (!type.string(data.dataSource)) {
+            response.status(400).send('Missing parameter \'dataSource\' in request body!');
+        } else if (!type.string(data.dataSet)) {
+            response.status(400).send('Missing parameter \'dataSet\' in request body!');
+        } else if (!this.dataSources.has(data.dataSource)) {
+            response.status(404).send(`The data source ${data.dataSource} was not found!`);
+        } else {
             const clusterIdentifier = `${data.dataSource}/${data.dataSet}`;
 
             // resolve the cluster service
             const clusterHost = await this.registryClient.resolve('rda-cluster');
 
             // check the status on the cluster service
-            log.info(`Checking if the cluster to be created exists already`);
-            const clusterStatusResponse = await superagent.get(`${clusterHost}/rda-cluster.cluster/${clusterIdentifier}`).ok(res => [404, 200].includes(res.status)).send();
+            log.info('Checking if the cluster to be created exists already');
+            const clusterStatusURL = `${clusterHost}/rda-cluster.cluster/${clusterIdentifier}`;
+            const clusterStatusResponse = await superagent.get(clusterStatusURL)
+                .ok(res => [404, 200].includes(res.status))
+                .send();
 
             // make sure neither we, nor the cluster service knows
             // a thing about the cluster to be created
-            if (clusterStatusResponse.status === 404 && !this.clusterStatus.has(clusterIdentifier)) {
+            if (clusterStatusResponse.status === 404 &&
+                !this.clusterStatus.has(clusterIdentifier)) {
 
                 // get the service address of the data source
                 const storageHost = await this.registryClient.resolve(data.dataSource);
 
-                // request information about the data that will 
-                // be processed by the cluster. this is needed 
-                // to size the cluster correctly
-                log.info(`Getting information about the data that has to be loaded into the cluster`);
-                const dataInfoResponse = await superagent.get(`${storageHost}/${data.dataSource}.dataset-info/${data.dataSet}`).ok(res => res.status === 200).send();
+
+                // request information about the data that will be processed by the cluster. this is
+                // needed to size the cluster correctly
+                log.info('Getting information about the data that has to be loaded into the cluster');
+                const dataInfoURL = `${storageHost}/${data.dataSource}.dataset-info/${data.dataSet}`;
+                const dataInfoResponse = await superagent.get(dataInfoURL)
+                    .ok(res => res.status === 200)
+                    .send();
+
                 const info = dataInfoResponse.body;
 
-                // create the cluster 
-                log.info(`Setting up the cluster`);
-                const clusterResponse = await superagent.post(`${clusterHost}/rda-cluster.cluster`).ok(res => res.status === 201).send({
-                    requiredMemory: info.totalMemory,
-                    recordCount: info.recordCount,
-                    dataSet: data.dataSet,
-                    dataSource: data.dataSource,
-                });
+
+
+                // create the cluster
+                log.info('Setting up the cluster');
+                const clusterResponse = await superagent.post(`${clusterHost}/rda-cluster.cluster`)
+                    .ok(res => res.status === 201)
+                    .send({
+                        requiredMemory: info.totalMemory,
+                        recordCount: info.recordCount,
+                        dataSet: data.dataSet,
+                        dataSource: data.dataSource,
+                    });
+
+
 
                 // instruct the data source to create the shards
-                log.info(`Instruct the datasource to create data shards`);
-                await superagent.post(`${storageHost}/${data.dataSource}.shard`).ok(res => res.status === 201).send({
-                    shards: clusterResponse.body.shards,
-                    dataSet: data.dataSet,
-                });
+                log.info('Instruct the datasource to create data shards');
+                await superagent.post(`${storageHost}/${data.dataSource}.shard`)
+                    .ok(res => res.status === 201)
+                    .send({
+                        shards: clusterResponse.body.shards,
+                        dataSet: data.dataSet,
+                    });
+
 
                 // tell the cluster to initialize
-                log.info(`Initialize cluster`);
-                await superagent.patch(`${clusterHost}/rda-cluster.cluster/${clusterResponse.body.clusterId}`).ok(res => res.status === 200).send();
+                log.info('Initialize cluster');
+                const { clusterId } = clusterResponse.body;
+                const clusterInitilizeURL = `${clusterHost}/rda-cluster.cluster/${clusterId}`;
+                await superagent.patch(clusterInitilizeURL)
+                    .ok(res => res.status === 200)
+                    .send();
 
-                // return some info. the user has to get the 
-                // status info using the lsitOne method 
+
+                // return some info. the user has to get the status info using the lsitOne method
                 return {
                     clusterId: clusterResponse.body.clusterId,
                     clusterIdentifier: clusterResponse.body.clusterIdentifier,
